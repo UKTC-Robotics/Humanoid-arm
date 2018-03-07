d1340cceaa052f3a41b28b2478275f986bda119c
Vencislav Nachev <vbn_94@abv.bg>
2018-03-07 22:46:32 +0200

    code formatting
    only formatting

--- ruka/ruka.ino
+++ ruka/ruka.ino
@@ -1,10 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
 
 #include <Servo.h>
 String stat;
@@ -18,44 +11,46 @@ void setup() {
   prusti[2].attach(A0);
   prusti[3].attach(A4);
   prusti[4].attach(A5);
+ 
   for(int i=0;i<sizeof pos/sizeof pos[0];i++){
-  pos[i]=130;
-  prusti[i].write(pos[i]);
-}
-  Serial.begin(19200);// attaches the servo on pin 9 to the servo object
+    pos[i]=130;
+    prusti[i].write(pos[i]);
+  }
+  Serial.begin(19200);
   Serial.println("I am ready!");
 }
 
 void loop() {
-if(Serial.available()>0)
-{
-  stat=Serial.readStringUntil(':');
-  switch(stat[0]){
-    case 'A':pos[0]=130;break;
-    case 'a':pos[0]=40;break;
-  }
-  switch(stat[1]){
-    case 'B':pos[1]=40;break;
-    case 'b':pos[1]=130;break;
-  }
-  switch(stat[2]){
-    case 'C':pos[2]=25;break;
-    case 'c':pos[2]=130;break;
-  }
-  switch(stat[3]){
-    case 'D':pos[3]=50;break;
-    case 'd':pos[3]=140;break;
-  }
-  switch(stat[4]){
-    case 'E':pos[4]=60;break;
-    case 'e':pos[4]=130;break;
-  }
-}
-for(int i=0;i<sizeof pos/sizeof pos[0];i++){
-prusti[i].write(pos[i]);
-Serial.print(pos[i]);
-Serial.print("|");
-}
-Serial.println();
-delay(5);
+ if(Serial.available()>0){
+   stat=Serial.readStringUntil(':');
+   switch(stat[0]){
+     case 'A':pos[0]=130;break;
+     case 'a':pos[0]=40;break;
+   }
+   switch(stat[1]){
+     case 'B':pos[1]=40;break;
+     case 'b':pos[1]=130;break;
+   }
+   switch(stat[2]){
+     case 'C':pos[2]=25;break;
+     case 'c':pos[2]=130;break;
+   }
+   switch(stat[3]){
+     case 'D':pos[3]=50;break;
+     case 'd':pos[3]=140;break;
+   }
+   switch(stat[4]){
+     case 'E':pos[4]=60;break;
+     case 'e':pos[4]=130;break;
+   }
+ }
+
+ for(int i=0;i<sizeof pos/sizeof pos[0];i++){
+   prusti[i].write(pos[i]);
+   Serial.print(pos[i]);
+   Serial.print("|");
+ }
+
+ Serial.println();
+ delay(5);
 } 
